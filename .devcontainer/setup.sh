316c0833960811324afac8aa67f3945ee227fc73
underscope <underscope@gmail.com>
2025-06-04 14:58:03 +0200

    🔧

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,11 +3,5 @@
 # the port is private from Github and Gitpod parts.
 # @see https://github.com/community/community/discussions/15351
 #!/bin/bash
-sleep 10
-if [ -n "$CODESPACES" ]; then
-    gh codespace ports visibility 8030:public -c $CODESPACE_NAME > /dev/null 2>&1
-elif [ -n "$GITPOD_WORKSPACE_URL" ]; then
-    gp ports visibility 8030:public > /dev/null 2>&1
-else
-    echo "Neither CODESPACES or GITPOD_WORKSPACE_URL environment variable is set"
-fi
+gh codespace ports visibility 8030:public -c $CODESPACE_NAME
+exit 0
